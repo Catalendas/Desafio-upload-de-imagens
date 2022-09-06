a8670779e74d4330babf3a851f6f0418cabb4534
MarcosAlexandre <82763928+Catalendas@users.noreply.github.com>
2022-09-06 19:22:39 -0300

    Projeto finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,31 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+  interface Image {
+    title: string
+    description: string
+    url: string
+    ts: number
+    id: string
+  }
+
+  interface GetImagesResponse {
+    after: string
+    data: Image[]
+  }
+
 export default function Home(): JSX.Element {
+
+  async function fetchImages({pageParam = null}): Promise<GetImagesResponse> {
+    const {data} = await api('/api/images', {
+      params: {
+        after: pageParam
+      }
+    })
+
+    return data
+  }
+
   const {
     data,
     isLoading,
@@ -17,19 +41,29 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    'images', fetchImages, {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat()
+    })
+
+    console.log(data?.pages)
+    console.log(formatted)
+
+    return formatted
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />
+  }
 
   return (
     <>
@@ -37,7 +71,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage} mt="6">
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
